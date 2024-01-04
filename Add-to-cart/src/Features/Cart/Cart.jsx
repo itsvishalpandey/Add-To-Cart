1457270c317aa8485cc23fad9c853a78d5ea668e
itsvishalpandey <itsvishalpandey16@gmail.com>
2024-01-04 21:46:57 +0530

    added filter cards

--- Add-to-cart/src/Features/Cart/Cart.jsx
+++ Add-to-cart/src/Features/Cart/Cart.jsx
@@ -1,23 +1,38 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import CartPage from "./CartCard";
 import { useGames } from "./GameApi";
 
 function Cart() {
   const games = useGames();
-  console.log(games);
+
+  const [search, setSearch] = useState("");
+
+  console.log(search);
 
   return (
     <>
       <div className="p-4 ">
-        <div>
-          <h1 className="text-2xl text-center font-bold py-2">
-            All the characters
-          </h1>
-        </div>
+        <form className="w-2/6 mx-auto  border border-black rounded flex my-4">
+          <input
+            className="w-full outline-none py-1 px-2 rounded-l"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            type="text"
+            placeholder="Search here"
+          />
+          <button className="px-4 py-1 bg-blue-300 rounded-r">Search</button>
+        </form>
+
         <div className="w-11/12 mx-auto flex flex-wrap gap-6 px-2 py-6">
-          {games.map((character, index) => (
-            <CartPage key={index} character={character} />
-          ))}
+          {games
+            .filter((character) =>
+              search.toLowerCase() === ""
+                ? character
+                : character.type.toLowerCase().includes(search)
+            )
+            .map((character, index) => (
+              <CartPage key={index} character={character} />
+            ))}
         </div>
       </div>
     </>
